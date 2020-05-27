2f5a1041c9dd0606647d4497729ea3d482f73b37
Gostyu <cedric.lissossi@gmail.com>
2020-05-28 00:59:12 +0200

    First Flutter App : i add some package

--- flutter_101/lib/main.dart
+++ flutter_101/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("bienvenue"),
-));
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final wordPair=WordPair.random();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text("First App"),
+          ),
+          body: Center(
+            child: Text(wordPair.asPascalCase)
+            ),
+      ),
+    );
+  }
+}
