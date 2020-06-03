c75c64cfa6f4eede0becb062c471871153b5a9d2
Gostyu <cedric.lissossi@gmail.com>
2020-06-03 13:51:55 +0200

    flutter 102 :navigation to a new screen added

--- flutter_101/lib/main.dart
+++ flutter_101/lib/main.dart
@@ -7,18 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title:Text("Word Generator")
-          ),
-          body: RandomWords(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: null,
-              child:Text("click"),
-              backgroundColor: Colors.blue,
-               )
-      ),
+    return MaterialApp (
+      title: "Word generator app",
+      home:RandomWords(),
     );
   }
 }
@@ -31,7 +22,27 @@ class RandomWordState extends State<RandomWords>{
   final List<WordPair> _words = <WordPair>[] ;
   final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize:18);
-
+  
+  void _pushSaved(){//go to the fav word screen 
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (BuildContext ctx){
+            final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair){
+                  return ListTile(title: Text(pair.asPascalCase,style:_biggerFont)); 
+                },
+            );
+            final List<Widget> divided = ListTile.divideTiles(context:ctx,
+            tiles:tiles,).toList();
+         return Scaffold(appBar: AppBar(
+            title: Text("Saved words"),
+            ),
+            body: ListView(children:divided)
+          );
+         },
+      )
+    );
+  }
   Widget buildSuggestions(){
     return ListView.builder(itemBuilder: (BuildContext _ctx, int i){
       if(i.isOdd){return Divider();}
@@ -50,7 +61,7 @@ class RandomWordState extends State<RandomWords>{
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
-      onTap: ()=>{
+      onTap: ()=>
         setState((){
           if(alreadySaved){
             _saved.remove(pair);
@@ -60,12 +71,19 @@ class RandomWordState extends State<RandomWords>{
             print("$pair is added");
           }
         })
-      },
+      ,
     );
   }
   @override
-      Widget build(BuildContext context) {
-        return buildSuggestions();
-      }      
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      title: Text("Word generator"),
+      actions: <Widget>[
+        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+      ],
+    ),
+      body: buildSuggestions(),
+    );    
+  }      
 }
 
