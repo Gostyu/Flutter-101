f0fb0a753393984c12f7fc068611ad4ca404f970
Gostyu <cedric.lissossi@gmail.com>
2020-06-03 11:50:10 +0200

    flutter 101:i add a stateful widget

--- flutter_101/lib/main.dart
+++ flutter_101/lib/main.dart
@@ -7,16 +7,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair=WordPair.random();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title:Text("First App"),
+          title:Text("First App")
           ),
-          body: Center(
-            child: Text(wordPair.asPascalCase)
-            ),
+          body: RandomWords(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: null,
+              child:Text("click"),
+              backgroundColor: Colors.blue,
+               )
       ),
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordState createState() => RandomWordState();
+}
+class RandomWordState extends State<RandomWords>{
+  
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = WordPair.random();
+    return  Center(
+            child: Text(wordPair.asPascalCase),
+            );
+  }
+
 }
